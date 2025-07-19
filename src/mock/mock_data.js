20b152060a9829612dc0894f9232e60b3206306b
Zmin <104510233+Zmin2003@users.noreply.github.com>
2025-07-19 18:05:28 +0800

    Update mock_data.js

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -1,178 +1,179 @@
 export const mockData = {
-  categories: [
+  "categories": [
     {
-      id: "my-favorites",
-      name: "我的常用",
-      icon: "💥",
-      order: 0,
-      sites: [
+      "id": "my-favorites",
+      "name": "我的常用",
+      "icon": "💥",
+      "order": 0,
+      "sites": [
         {
-          id: "论坛",
-          name: "论坛",
-          url: "https://linux.do/",
-          description: "",
-          icon: "https://icon.maodeyu.fun/favicon/linux.do"
+          "id": "linux-do",
+          "name": "论坛",
+          "url": "https://linux.do/",
+          "description": "",
+          "icon": "/sitelogo/linux.do.ico"
         },
         {
-          id: "github",
-          name: "github",
-          url: "https://github.com/",
-          description: "",
-          icon: "https://icon.maodeyu.fun/favicon/github.com"
+          "id": "github",
+          "name": "github",
+          "url": "https://github.com/",
+          "description": "",
+          "icon": "/sitelogo/github.com.ico"
         },
         {
-          id: "zmin video",
-          name: "zmin video",
-          url: "https://tv.zmin.fyi/",
-          description: "",
-          icon: "https://icon.maodeyu.fun/favicon/tv.zmin.fyi"
+          "id": "zmin-video",
+          "name": "zmin video",
+          "url": "https://tv.zmin.fyi/",
+          "description": "",
+          "icon": "/sitelogo/tv.zmin.fyi.ico"
         },
         {
-          id: "4k影视-高清在线---4k影视",
-          name: "4k影视-高清在线 - 4k影视",
-          url: "https://www.4kvm.tv/",
-          description: "",
-          icon: "https://icon.maodeyu.fun/favicon/www.4kvm.tv"
+          "id": "4kvm-tv",
+          "name": "4k影视-高清在线 - 4k影视",
+          "url": "https://www.4kvm.tv/",
+          "description": "",
+          "icon": "/sitelogo/www.4kvm.tv.ico"
         }
       ]
     },
     {
-      id: "cloud",
-      name: "云服务",
-      icon: "☁️",
-      order: 2,
-      sites: [
+      "id": "cloud",
+      "name": "云服务",
+      "icon": "☁️",
+      "order": 2,
+      "sites": [
         {
-          id: "cf",
-          name: "cf",
-          url: "https://dash.cloudflare.com/",
-          description: "",
-          icon: "https://icon.maodeyu.fun/favicon/dash.cloudflare.com"
+          "id": "cf",
+          "name": "cf",
+          "url": "https://dash.cloudflare.com/",
+          "description": "",
+          "icon": "/sitelogo/dash.cloudflare.com.ico"
         },
         {
-          id: "webdev",
-          name: "webdev",
-          url: "https://infini-cloud.net/en/",
-          description: "",
-          icon: "https://icon.maodeyu.fun/favicon/infini-cloud.net"
+          "id": "webdev",
+          "name": "webdev",
+          "url": "https://infini-cloud.net/en/",
+          "description": "",
+          "icon": "/sitelogo/infini-cloud.net.ico"
         },
         {
-          id: "cats-api",
-          name: "Cats API",
-          url: "https://catsapi.com/pricing",
-          description: "",
-          icon: "https://icon.maodeyu.fun/favicon/catsapi.com"
+          "id": "cats-api",
+          "name": "Cats API",
+          "url": "https://catsapi.com/pricing",
+          "description": "",
+          "icon": "/sitelogo/catsapi.com.ico"
         },
         {
-          id: "new-api",
-          name: "New API",
-          url: "https://tbai.xin/",
-          description: "",
-          icon: "https://icon.maodeyu.fun/favicon/tbai.xin"
+          "id": "new-api",
+          "name": "New API",
+          "url": "https://tbai.xin/",
+          "description": "",
+          "icon": "/sitelogo/tbai.xin.ico"
         }
       ]
     },
     {
-      id: "dev-tools",
-      name: "开发工具",
-      icon: "🛠️",
-      order: 3,
-      sites: [
+      "id": "dev-tools",
+      "name": "开发工具",
+      "icon": "🛠️",
+      "order": 3,
+      "sites": [
         {
-          id: "github-docker-加速",
-          name: "GitHub docker 加速",
-          url: "https://cnb.cool/znb/hubproxy",
-          description: "",
-          icon: "https://icon.maodeyu.fun/favicon/cnb.cool"
+          "id": "github-docker-accelerator",
+          "name": "GitHub docker 加速",
+          "url": "https://cnb.cool/znb/hubproxy",
+          "description": "",
+          "icon": "/sitelogo/cnb.cool.ico"
         },
         {
-          id: "vercel",
-          name: "vercel",
-          url: "https://vercel.com/bantianjingchuans-projects/",
-          description: "",
-          icon: "https://icon.maodeyu.fun/favicon/vercel.com"
+          "id": "vercel",
+          "name": "vercel",
+          "url": "https://vercel.com/bantianjingchuans-projects/",
+          "description": "",
+          "icon": "/sitelogo/vercel.com.ico"
         },
         {
-          id: "github-时间脚本",
-          name: "GitHub 时间脚本",
-          url: "https://docs.rational-stars.top/",
-          description: "",
-          icon: "https://icon.maodeyu.fun/favicon/docs.rational-stars.top"
+          "id": "github-time-script",
+          "name": "GitHub 时间脚本",
+          "url": "https://docs.rational-stars.top/",
+          "description": "",
+          "icon": "/sitelogo/docs.rational-stars.top.ico"
         },
         {
-          id: "csdn会员文章解锁",
-          name: "CSDN会员文章解锁",
-          url: "https://linux.do/t/topic/758801",
-          description: "",
-          icon: "https://icon.maodeyu.fun/favicon/linux.do"
+          "id": "csdn-unlock",
+          "name": "CSDN会员文章解锁",
+          "url": "https://linux.do/t/topic/758801",
+          "description": "",
+          "icon": "/sitelogo/linux.do.ico"
         }
       ]
     },
     {
-      id: "learning",
-      name: "学习资源",
-      icon: "📚",
-      order: 6,
-      sites: [
+      "id": "learning",
+      "name": "学习资源",
+      "icon": "📚",
+      "order": 6,
+      "sites": [
         {
-          id: "重庆科创",
-          name: "重庆科创",
-          url: "https://i.mooc.chaoxing.com/space/index?t=1711770764305",
-          description: "",
-          icon: "https://icon.maodeyu.fun/favicon/i.mooc.chaoxing.com"
+          "id": "chaoxing-space",
+          "name": "重庆科创",
+          "url": "https://i.mooc.chaoxing.com/space/index?t=1711770764305",
+          "description": "",
+          "icon": "/sitelogo/i.mooc.chaoxing.com.ico"
         },
         {
-          id: "学习通助手",
-          name: "学习通助手",
-          url: "https://greasyfork.cn/post/8",
-          description: "",
-          icon: "https://icon.maodeyu.fun/favicon/greasyfork.cn"
+          "id": "xuetong-helper",
+          "name": "学习通助手",
+          "url": "https://greasyfork.cn/post/8",
+          "description": "",
+          "icon": "/sitelogo/greasyfork.cn.ico"
         }
       ]
     },
     {
-      id: "tools",
-      name: "工具",
-      icon: "⚙️",
-      order: 7,
-      sites: [
+      "id": "tools",
+      "name": "工具",
+      "icon": "⚙️",
+      "order": 7,
+      "sites": [
         {
-          id: "消防百事通",
-          name: "消防百事通",
-          url: "https://www.fire114.cn/",
-          description: "",
-          icon: "https://icon.maodeyu.fun/favicon/www.fire114.cn"
+          "id": "fire114",
+          "name": "消防百事通",
+          "url": "https://www.fire114.cn/",
+          "description": "",
+          "icon": "/sitelogo/www.fire114.cn.ico"
         },
         {
-          id: "域名",
-          name: "域名",
-          url: "https://porkbun.com/",
-          description: "",
-          icon: "https://icon.maodeyu.fun/favicon/porkbun.com"
+          "id": "porkbun",
+          "name": "域名",
+          "url": "https://porkbun.com/",
+          "description": "",
+          "icon": "/sitelogo/porkbun.com.ico"
         },
         {
-          id: "vps-ls",
-          name: "VPS ls",
-          url: "https://vpsls.com/docs/%E9%A6%96%E9%A1%B5/",
-          description: "",
-          icon: "https://icon.maodeyu.fun/favicon/vpsls.com"
+          "id": "vps-ls",
+          "name": "VPS ls",
+          "url": "https://vpsls.com/docs/%E9%A6%96%E9%A1%B5/",
+          "description": "",
+          "icon": "/sitelogo/vpsls.com.ico"
         },
         {
-          id: "你好HR",
-          name: "你好HR",
-          url: "https://chromewebstore.google.com/detail/%E4%BD%A0%E5%A5%BDhr/dogjfmeploffpakiffkgjjlfhkgcmhfc",
-          description: "",
-          icon: "https://icon.maodeyu.fun/favicon/chromewebstore.google.com"
+          "id": "nihaoh
+r",
+          "name": "你好HR",
+          "url": "https://chromewebstore.google.com/detail/%E4%BD%A0%E5%A5%BDhr/dogjfmeploffpakiffkgjjlfhkgcmhfc",
+          "description": "",
+          "icon": "/sitelogo/chromewebstore.google.com.ico"
         },
         {
-          id: "收件箱---jolyne_kojo",
-          name: "收件箱 - jolyne_kojo",
-          url: "https://email-web-app.j3.workers.dev/",
-          description: "",
-          icon: "https://icon.maodeyu.fun/favicon/email-web-app.j3.workers.dev"
+          "id": "inbox-jolyne-kojo",
+          "name": "收件箱 - jolyne_kojo",
+          "url": "https://email-web-app.j3.workers.dev/",
+          "description": "",
+          "icon": "/sitelogo/email-web-app.j3.workers.dev.ico"
         }
       ]
-    },
-  ]
+    }
+  ],
   "title": "猫猫导航"
 };
