e5cf534c0da0a0ce9966c19278be03e2f3e24acc
Zmin <104510233+Zmin2003@users.noreply.github.com>
2025-09-14 10:25:46 +0800

    chore: 更新导航数据 - 2025/9/14 10:25:45

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -48,6 +48,13 @@ export const mockData = {
           "url": "https://api.v36.cm/",
           "description": "GPT专供和综合",
           "icon": "/sitelogo/api.v36.cm.ico"
+        },
+        {
+          "id": "site-1757816740297",
+          "name": "cerebras",
+          "url": "https://www.cerebras.ai/",
+          "description": "高并发api服务",
+          "icon": ""
         }
       ]
     },
